d1ebd58cea73ec0cebbe754471b3b6e9d5a9ea3f
MichaMans <michael.mans@hotmail.com>
2017-06-13 18:19:15 +0200

    changed run

--- AixLib/Resources/Scripts/RunUnitTests.py
+++ AixLib/Resources/Scripts/RunUnitTests.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #######################################################
 # Script that runs all unit tests or, optionally,
-# only checks the html syntax.
+# only checks the html syntax or the validity of
+# the simulation parameters of the models
 #
 # To run the unit tests, this script
 # - creates temporary directories for each processor,
@@ -24,13 +25,21 @@
 # non-zero exit value.
 #
 # MWetter@lbl.gov                            2011-02-23
+# TSNouidui@lbl.gov                          2017-04-11
 #######################################################
 
-def _validate_html():
+
+def _validate_experiment_setup(path):
     import buildingspy.development.validator as v
 
     val = v.Validator()
-    errMsg = val.validateHTMLInPackage(".")
+    retVal = val.validateExperimentSetup(path)
+
+def _validate_html(path):
+    import buildingspy.development.validator as v
+
+    val = v.Validator()
+    errMsg = val.validateHTMLInPackage(path)
     n_msg = len(errMsg)
     for i in range(n_msg):
         if i == 0:
@@ -56,22 +65,26 @@ def _setEnvironmentVariables(var, value):
     else:
         os.environ[var] = value
 
-def _runUnitTests(batch, single_package, n_pro):
+def _runUnitTests(batch, package, path, n_pro, show_gui):
     import buildingspy.development.regressiontest as u
 
     ut = u.Tester()
     ut.batchMode(batch)
-    if single_package is not None:
-        ut.setSinglePackage(single_package)
+    ut.setLibraryRoot(path)
+    if package is not None:
+        ut.setSinglePackage(package)
     ut.setNumberOfThreads(n_pro)
     ut.pedanticModelica(True)
+    ut.showGUI(show_gui)
     # Below are some option that may occassionally be used.
     # These are currently not exposed as command line arguments.
 #    ut.setNumberOfThreads(1)
 #    ut.deleteTemporaryDirectories(False)
 #    ut.useExistingResults(['/tmp/tmp-Buildings-0-fagmeZ'])
 
+#    ut.writeOpenModelicaResultDictionary()
     # Run the regression tests
+
     retVal = ut.run()
     return retVal
 
@@ -99,15 +112,25 @@ if __name__ == '__main__':
     unit_test_group.add_argument('-s', "--single-package",
                         metavar="Modelica.Package",
                         help="Test only the Modelica package Modelica.Package")
+    unit_test_group.add_argument("-p", "--path",
+                        default = ".",
+                        help="Path where top-level package.mo of the library is located")
+
     unit_test_group.add_argument("-n", "--number-of-processors",
                         type=int,
                         default = multiprocessing.cpu_count(),
                         help='Maximum number of processors to be used')
+    unit_test_group.add_argument("--show-gui",
+                        help='Show the GUI of the simulator',
+                        action="store_true")
 
     html_group = parser.add_argument_group("arguments to check html syntax only")
     html_group.add_argument("--validate-html-only",
                            action="store_true")
 
+    experiment_setup_group = parser.add_argument_group("arguments to check validity of .mos and .mo experiment setup only")
+    experiment_setup_group.add_argument("--validate-experiment-setup",
+                           action="store_true")
 
     # Set environment variables
     if platform.system() == "Windows":
@@ -115,9 +138,13 @@ if __name__ == '__main__':
                                  os.path.join(os.path.abspath('.'),
                                               "Resources", "Library", "win32"))
     else:
+        # For https://github.com/lbl-srg/modelica-buildings/issues/559, we add
+        # 32 and 64 bit resources to run the Utilities.IO.Python27 regression tests.
         _setEnvironmentVariables("LD_LIBRARY_PATH",
                                  os.path.join(os.path.abspath('.'),
-                                              "Resources", "Library", "linux32"))
+                                              "Resources", "Library", "linux32") + ":" +
+                                 os.path.join(os.path.abspath('.'),
+                                              "Resources", "Library", "linux64"))
 
     # The path to buildingspy must be added to sys.path to work on Linux.
     # If only added to os.environ, the Python interpreter won't find buildingspy
@@ -129,7 +156,12 @@ if __name__ == '__main__':
 
     if args.validate_html_only:
         # Validate the html syntax only, and then exit
-        ret_val = _validate_html()
+        ret_val = _validate_html(args.path)
+        exit(ret_val)
+
+    if args.validate_experiment_setup:
+        # Match the mos file parameters with the mo files only, and then exit
+        ret_val = _validate_experiment_setup(args.path)
         exit(ret_val)
 
     if args.single_package:
@@ -138,8 +170,10 @@ if __name__ == '__main__':
         single_package = None
 
     retVal = _runUnitTests(batch = args.batch,
-                           single_package = single_package,
-                           n_pro = args.number_of_processors)
+                           package = single_package,
+                           path = args.path,
+                           n_pro = args.number_of_processors,
+                           show_gui = args.show_gui)
     exit(retVal)
 
 #   _runOpenModelicaUnitTests()
