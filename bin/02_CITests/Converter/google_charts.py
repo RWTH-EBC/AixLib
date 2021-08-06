dbbebfcb74e1dacc7fda53d33275b6d8c49a022c
SvenHinrichs <sven.hinrichs@eonerc.rwth-aachen.de>
2021-08-06 14:58:47 +0200

    Add Documentation and delte unused functions

--- bin/02_CITests/Converter/google_charts.py
+++ bin/02_CITests/Converter/google_charts.py
@@ -86,7 +86,7 @@ def read_data(ref_file): ## Read Reference results in AixLib\Resources\Reference
 	
 	
 	
-def func_plot(distriction_values, distriction_time, Value_List,X_Axis,ref_file):
+def func_plot(distriction_values, distriction_time, Value_List,X_Axis,ref_file): # Create plots with matplot
 	dia_name = ref_file.replace(".txt","")
 	dia_name = dia_name.replace("_",".")
 	
@@ -127,20 +127,6 @@ def func_plot(distriction_values, distriction_time, Value_List,X_Axis,ref_file):
 	
 	return lines, fig
 	
-def write_data_rows():
-	
-	print("testq")
-	for i, j in zip((1,2,3), (4,5,6)):
-		print (i, j)
-
-def _CloneRepository():
-	git_url = "https://github.com/ibpsa/modelica-ibpsa.git"
-	repo_dir = "modelica-ibpsa"
-	if os.path.exists(repo_dir):
-		print("IBPSA folder exists already!")
-	else:
-		print("Clone IBPSA Repo")
-		repo = Repo.clone_from(git_url, repo_dir)
 
 
 def createFolder(directory):
@@ -151,192 +137,12 @@ def createFolder(directory):
         print ('Error: Creating directory. ' +  directory)
         
 
-def diff_ref(path_aix,path_ibpsa,path_diff,path_new):
-	aix_ref_files = os.listdir(path_aix) 
-	ibpsa_ref_files = os.listdir(path_ibpsa) 
-	ibpsa_list = []
-	## Give double reference files
-	for i in ibpsa_ref_files:
-		i = i.replace("IBPSA","AixLib")
-		ibpsa_list.append(i)
-	
-	set1 = set(aix_ref_files)
-	set2 = set(ibpsa_list)
-	set3 = set1.intersection(set2)
-	list3 = list(set3)
-	
-	for f in list3:
-		aix_ref = f
-		ibpsa_ref = f.replace("AixLib","IBPSA")
-		
-		aix_ref = path_aix+os.sep+ aix_ref
-		ibpsa_ref = path_ibpsa+os.sep+ibpsa_ref
-		result = path_diff +os.sep+f
-		
-		
-		outputFile = open(result,"w")
-		inputFile1 = open(aix_ref,"r")
-		inputFile2 = open(ibpsa_ref,"r")
-		
-		line1 = inputFile1.readline()
-		line2 = inputFile2.readline()
-		
-		while line1 != "" or line2 != "":
-			if line1 != line2:
-				outputFile.write("AixLib: "+line1)
-				outputFile.write("IBPSA: "+line2+"\n")
-				
-			line1 = inputFile1.readline()
-			line2 = inputFile2.readline()	
-		inputFile1.close()
-		inputFile2.close()
-		outputFile.close()
 
-	for root,dirs,files in os.walk(path_diff):  
-		for name in files:  
-			filename = os.path.join(root,name)  
-			if os.stat(filename).st_size == 0:  
-				os.remove(filename)
-	
-	
-	diff_ref = os.listdir(path_diff)
-	new_ref =(set1^set2)&set2
-	
-	for i in new_ref:
-		i = i.replace("AixLib","IBPSA")
-		source = path_ibpsa+os.sep+i
-		shutil.copy2(source,path_new)
-		
-		
-	new_ref =os.listdir(path_new)
-	return diff_ref, new_ref
-	
-def get_lines(filename):
-    f = open(filename,"r")
-    lines = f.readline()
-    f.close
-    return lines
-
-
-def add_new_ref(diff_ref,new_ref,path_aix,path_ibpsa,path_new,path_diff):
-	new_ref_list = []
-	for i in new_ref:
-		source = path_new+os.sep+i
-		ibp = path_new+os.sep+i.replace("IBPSA","AixLib")
-		file = pathlib.Path(ibp)
-		
-		if file.exists ():
-			new_ref_list.append(file)
-			continue
-		
-		if i.find("IBPSA")> -1 :
-			i = i.replace("IBPSA","AixLib")
-			i = path_new+os.sep+i
-			os.rename(source, i)
-		else:
-			i = path_new+os.sep+i
-		new_ref_list.append(i)
-	
-	## Copy New Files
-	for i in new_ref_list:
-		path = path_aix
-		shutil.copy2(i,path)
-	
-	for i in diff_ref:
-		path = path_aix
-		i = i.replace("AixLib","IBPSA")
-		source = path_ibpsa+os.sep+i
-		target = path+os.sep+i.replace("IBPSA","AixLib")
-		shutil.copy2(source,target)
-		
-	
-#def delte_ibpsa_dir():
-	
-	'''
-	for i in new_ref:
-		#i = i.replace("IBPSA","AixLib")
-		new_ref_list.append(i)
-	for k in diff_ref:
-		#k = i.replace("AixLib","IBPSA")
-		new_ref_list.append(k)
-	#print(new_ref_list)
-	
-	# Delte AixLib Reference Files
-	for l in diff_ref:
-		file = path_aix+os.sep+l
-		os.remove(file) 
-	# Add new and updated reference files
-	#for 
-	#shutil.copy2(source,path_new)'''
-		
-def func(label):
+def func(label): ## For Matplot Plots: Create a Hitbox for different variables
 	index = labels.index(label)
 	lines[index].set_visible(not lines[index].get_visible())
 	plt.draw()
 
-def diff_mos(path_aix_mos,path_ibpsa_mos,path_diff_mos,path_new_mos):
-	
-	aix_mos_files = glob.glob(path_aix_mos+os.sep+'**/*.mos',recursive=True)
-	ibpsa_mos_files = glob.glob(path_ibpsa_mos+os.sep+'**/*.mos',recursive=True)
-	
-	ibpsa_list = []
-	aixlib_list = []
-	## Give double mos files
-	for i in ibpsa_mos_files:
-		ibpsa_list.append(i.split(os.sep)[-1])
-	for i in aix_mos_files:
-		aixlib_list.append(i.split(os.sep)[-1])
-	
-	
-	set1 = set(aixlib_list)
-	set2 = set(ibpsa_list)
-	set3 = set1.intersection(set2)
-	list3 = list(set3)
-	for i in list3:
-		aixlib = glob.glob(path_aix_mos+os.sep+'**/*'+os.sep+i,recursive=True)
-		ibpsa = glob.glob(path_ibpsa_mos+os.sep+'**/*'+os.sep+i,recursive=True)
-		for l in aixlib:
-			aix = l
-			ibpsa = "modelica-ibpsa"+os.sep+l.replace("AixLib","IBPSA")
-			result = path_diff_mos +os.sep+l
-			result = (result.replace(os.sep+"AixLib"+os.sep+"Resources"+os.sep+"Scripts"+os.sep+"Dymola",""))
-			createFolder(result[:result.rfind(os.sep)])
-		
-			outputFile = open(result,"w")
-			inputFile1 = open(aix,"r")
-			inputFile2 = open(ibpsa,"r")
-			
-			line1 = inputFile1.readline()
-			line2 = inputFile2.readline()
-			line3 = line2.replace("IBPSA","AixLib")
-			while line1 != "" or line3 != "":
-				if line1 != line3:
-					outputFile.write("AixLib: "+line1)
-					outputFile.write("IBPSA: "+line2+"\n")
-					
-				line1 = inputFile1.readline()
-				line3 = inputFile2.readline()	
-			inputFile1.close()
-			inputFile2.close()
-			outputFile.close()
-			#print(result)
-			if os.stat(result).st_size == 0:  
-				os.remove(result)
-			
-	new_ref =(set1^set2)&set2
-	new_mos_list = []
-	for i in new_ref:
-		if i.find("ConvertIBPSA") > -1 :
-			continue
-		else:
-			ibpsa = glob.glob(path_ibpsa_mos+os.sep+'**/*'+os.sep+i,recursive=True)
-			new_mos_list.append(ibpsa)
-			source = path_new_mos+os.sep+i
-			for l in ibpsa:
-				shutil.copy2(l,source)
-	
-	
-	
 
 def removeEmptyFolders(path, removeRoot=True):
   #'Function to remove empty folders'
@@ -349,7 +155,7 @@ def removeEmptyFolders(path, removeRoot=True):
     for f in files:
       fullpath = os.path.join(path, f)
       if os.path.isdir(fullpath):
-        removeEmptyFolders(fullpath)
+        os.rmdir(fullpath)
 
   # if folder empty, delete it
   files = os.listdir(path)
@@ -363,7 +169,7 @@ def usageString():
 
 
 
-def read_unitTest_log(f_log):
+def read_unitTest_log(f_log): ## Read unitTest_log from regressionTest, write variable and modelname with difference
 	path = "AixLib"+os.sep+"funnel_comp"
 	log = open(f_log,"r")
 	lines = log.readlines()
@@ -384,7 +190,7 @@ def read_unitTest_log(f_log):
 
 	return var_dic, path_list, var_list
 	
-def sort_mo_var(dic):
+def sort_mo_var(dic): ## Search for variables in referencefiles
 	ref_path = "AixLib"+os.sep+"Resources"+os.sep+"ReferenceResults"+os.sep+"Dymola"
 	mo_list = []
 	var_mod_dic = {}
@@ -397,7 +203,7 @@ def sort_mo_var(dic):
 				var_mod_dic[ref_path+os.sep+file] = dic[l]
 	return  var_mod_dic
 
-def read_csv_funnel(url,csv_file, test_csv):
+def read_csv_funnel(url,csv_file, test_csv): ## Read the differenz variables from csv_file and test_file
 	# Parameter
 	csv_file = url.strip()+os.sep+csv_file
 	test_csv = url.strip()+os.sep+test_csv
@@ -429,7 +235,7 @@ def read_csv_funnel(url,csv_file, test_csv):
 		print(csv_file + "is empty")
 
 
-def mako_line_html_chart(data,temp,temp_chart,f_log,csv_file,test_csv):
+def mako_line_html_chart(data,temp,temp_chart,f_log,csv_file,test_csv): # Load and read the templates , write variables in the templates
 	from mako.template import Template
 	green = "\033[0;32m"
 	CRED = '\033[91m'
@@ -469,7 +275,7 @@ def mako_line_html_chart(data,temp,temp_chart,f_log,csv_file,test_csv):
 			file_tmp = open(html, "w")
 			file_tmp.write(hmtl_chart)
 			file_tmp.close()
-def create_index_layout(temp_chart):
+def create_index_layout(temp_chart): ## Create a index layout from a template
 	temp = "bin" + os.sep + "02_CITests" + os.sep + "Converter" + os.sep + "01_templates" + os.sep + "index.txt"
 	from mako.template import Template
 	html_model = []
@@ -489,7 +295,8 @@ def create_index_layout(temp_chart):
 	file_tmp = open(html, "w")
 	file_tmp.write(hmtl_chart)
 	file_tmp.close()
-def create_layout(index_path):
+
+def create_layout(index_path): ## Creates a layout index that has all links to the subordinate index files
 	temp = "bin" + os.sep + "02_CITests" + os.sep + "Converter" + os.sep + "01_templates" + os.sep + "layout_index.txt"
 	folder = (os.listdir(index_path))
 	package_list = []
@@ -515,6 +322,7 @@ def create_layout(index_path):
 		file_tmp.close()
 
 if  __name__ == '__main__':
+	# Set colors
 	green = "\033[0;32m"
 	CRED = '\033[91m'
 	CEND = '\033[0m'
@@ -563,16 +371,17 @@ if  __name__ == '__main__':
 	args = parser.parse_args()
 
 	# *********************************************************************************************************
+	# Set files for informations, templates and storage locations
 	csv_file = "reference.csv"
 	test_csv = "test.csv"
-	temp = "bin" + os.sep + "02_CITests" + os.sep + "Converter" + os.sep + "01_templates" + os.sep + "google_chart.txt"
+	temp = "bin" + os.sep + "02_CITests" + os.sep + "Converter" + os.sep + "01_templates" + os.sep + "google_chart.txt" ## path for google chart template
 
-	index_path = "bin" + os.sep + "03_WhiteLists" + os.sep + "charts"
-	f_log = "AixLib" + os.sep + "unitTests-dymola.log"
+	index_path = "bin" + os.sep + "03_WhiteLists" + os.sep + "charts" ## path for layout index
+	f_log = "AixLib" + os.sep + "unitTests-dymola.log" ## path for unitTest-dymola.log, important for errors
+
 	## Create Line chart html
-
 	if args.line_html is True:
-		temp_chart = "bin" + os.sep + "03_WhiteLists" + os.sep + "charts" + os.sep + args.single_package
+		temp_chart = "bin" + os.sep + "03_WhiteLists" + os.sep + "charts" + os.sep + args.single_package # path for every single package
 
 		# Plot all Datas with an error
 		if args.error is True:
@@ -820,7 +629,6 @@ if  __name__ == '__main__':
 	## Create Line matplot chart
 	if args.line_matplot is True:
 		print("Plot matplot Chart")
-		# write_data_rows()
 		results = read_data()
 		## Value Number with Legend
 		distriction_values = results[0]
