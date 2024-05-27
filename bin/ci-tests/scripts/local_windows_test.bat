98ebf13a6610e5eac9a7b2c1f8ed76405ffd42ad
fabian.wuellhorst <fabian.wuellhorst@eonerc.rwth-aachen.de>
2024-05-27 08:34:44 +0200

    dont test examples package which does not exist

--- bin/ci-tests/scripts/local_windows_test.bat
+++ bin/ci-tests/scripts/local_windows_test.bat
@@ -3,8 +3,8 @@
 :: .custom_install_additional_modelica_libraries
 :: .github_ssh_auth
 
-python -m ModelicaPyCI.syntax.html_tidy --filter-whitelist-flag  --correct-view-flag  --log-flag  --whitelist-library IBPSA --library AixLib --packages Airflow BoundaryConditions Controls DataBase Electrical Examples Fluid Media Obsolete Resources Systems ThermalZones Types UsersGuide Utilities 
-python -m ModelicaPyCI.syntax.html_tidy --filter-whitelist-flag  --correct-view-flag  --log-flag  --whitelist-library IBPSA --library AixLib --packages Airflow BoundaryConditions Controls DataBase Electrical Examples Fluid Media Obsolete Resources Systems ThermalZones Types UsersGuide Utilities 
+python -m ModelicaPyCI.syntax.html_tidy --filter-whitelist-flag  --correct-view-flag  --log-flag  --whitelist-library IBPSA --library AixLib --packages Airflow BoundaryConditions Controls DataBase Electrical Fluid Media Obsolete Resources Systems ThermalZones Types UsersGuide Utilities 
+python -m ModelicaPyCI.syntax.html_tidy --filter-whitelist-flag  --correct-view-flag  --log-flag  --whitelist-library IBPSA --library AixLib --packages Airflow BoundaryConditions Controls DataBase Electrical Fluid Media Obsolete Resources Systems ThermalZones Types UsersGuide Utilities 
 
 python -m ModelicaPyCI.syntax.style_checking --dymola-version 2022 --library AixLib 
 python -m ModelicaPyCI.syntax.style_checking --changed-flag  --dymola-version 2022 --library AixLib 
@@ -29,10 +29,6 @@ python -m ModelicaPyCI.unittest.checkpackages.validatetest --root-whitelist-libr
 python -m ModelicaPyCI.unittest.checkpackages.validatetest --root-whitelist-library .. --whitelist-library IBPSA --git-url https://github.com/ibpsa/modelica-ibpsa.git --dym-options DYM_SIM DYM_CHECK --dymola-version 2022 --additional-libraries-to-load  --library AixLib  --packages Electrical
 :: Check & Simulate AixLib Electrical on push
 python -m ModelicaPyCI.unittest.checkpackages.validatetest --root-whitelist-library .. --whitelist-library IBPSA --git-url https://github.com/ibpsa/modelica-ibpsa.git --dym-options DYM_SIM DYM_CHECK --changed-flag  --dymola-version 2022 --additional-libraries-to-load  --library AixLib  --packages Electrical
-:: Check & Simulate AixLib Examples on PR
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --root-whitelist-library .. --whitelist-library IBPSA --git-url https://github.com/ibpsa/modelica-ibpsa.git --dym-options DYM_SIM DYM_CHECK --dymola-version 2022 --additional-libraries-to-load  --library AixLib  --packages Examples
-:: Check & Simulate AixLib Examples on push
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --root-whitelist-library .. --whitelist-library IBPSA --git-url https://github.com/ibpsa/modelica-ibpsa.git --dym-options DYM_SIM DYM_CHECK --changed-flag  --dymola-version 2022 --additional-libraries-to-load  --library AixLib  --packages Examples
 :: Check & Simulate AixLib Fluid on PR
 python -m ModelicaPyCI.unittest.checkpackages.validatetest --root-whitelist-library .. --whitelist-library IBPSA --git-url https://github.com/ibpsa/modelica-ibpsa.git --dym-options DYM_SIM DYM_CHECK --dymola-version 2022 --additional-libraries-to-load  --library AixLib  --packages Fluid
 :: Check & Simulate AixLib Fluid on push
